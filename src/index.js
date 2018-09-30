36148eabee7a492dba12fa40dca94cd0ee2ee04d
zzVIPz <viperexe@mail.ru>
2018-09-30 23:05:23 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  n = 1;
+  count = 0;
+  while (number / Math.pow(5, n) >= 1) {
+
+    count += (Math.trunc(number / Math.pow(5, n)));
+    n++;
+  }
+  return (count);
 }
